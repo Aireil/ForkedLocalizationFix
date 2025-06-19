b3772e003452c77efc2acd1e7a4fad9b529d4590
Aireil <33433913+Aireil@users.noreply.github.com>
2025-06-19 14:59:15 +0200

    build: 0.0.0.1

---
+++ ForkedLocalizationFix/Plugin.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Dalamud.Hooking;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+using Dalamud.Plugin.Services;
+using Dalamud.Utility.Signatures;
+using FFXIVClientStructs.FFXIV.Client.System.Framework;
+
+namespace ForkedLocalizationFix;
+
+public sealed unsafe class ForkedLocalizationFix : IDalamudPlugin
+{
+    [PluginService] internal static IDataManager DataManager { get; private set; } = null!;
+    [PluginService] internal static IPluginLog Log { get; private set; } = null!;
+    [PluginService] internal static IGameInteropProvider GameInteropProvider { get; private set; } = null!;
+
+    private readonly nint goodRowPtr;
+    private readonly nint badRowPtr;
+
+    public ForkedLocalizationFix()
+    {
+        GameInteropProvider.InitializeFromAttributes(this);
+
+        var gameVersion = Framework.Instance()->GameVersionString;
+        var luminaGameVersion = DataManager.GameData.Repositories["ffxiv"].Version;
+        const string targetGameVersion = "2025.06.10.0000.0000";
+
+        if (gameVersion != targetGameVersion || luminaGameVersion != targetGameVersion)
+        {
+            return;
+        }
+
+        this.goodRowPtr = getActionIdRowPtr(41549);
+        this.badRowPtr = getActionIdRowPtr(41547);
+
+        this.resolveStringHook?.Enable();
+    }
+
+    public void Dispose()
+    {
+        this.resolveStringHook.Disable();
+    }
+
+    [Signature("E8 ?? ?? ?? ?? 80 FB 12")]
+    private static delegate* unmanaged<uint, nint> getActionIdRowPtr;
+
+    [Signature("E8 ?? ?? ?? ?? 8D 2C 5B", DetourName = nameof(ResolveStringDetour))]
+    private Hook<ResolveStringDelegate> resolveStringHook;
+    private delegate long ResolveStringDelegate(nint a1);
+    private long ResolveStringDetour(nint a1)
+    {
+        var ret = 0L;
+
+        try
+        {
+            ret = this.resolveStringHook.Original(a1 == badRowPtr ? goodRowPtr : a1);
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "Exception in ResolveStringDetour");
+        }
+
+        return ret;
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+This plugin fixes in/out cast names in French and German for the last boss of The Forked Tower. No configuration, it just works.
+
+It will automatically disable itself on any game update.
